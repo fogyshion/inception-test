6e84e8ca33ec131c186b33a135b69b1f1dfc86fd
fogyshion <fogyshion@163.com>
2025-07-07 20:57:24 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from plato.servers import fedavg
+from plato.clients import simple
+from plato.trainers import basic
+
+
+def main():
+    trainer = basic.Trainer
+    client = simple.Client(trainer=trainer)
+    server = fedavg.Server(trainer=trainer)
+    server.run(client)
+
+if __name__ == "__main__":
+    main()
